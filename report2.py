b5d86daab4b2e073adf4b10eef9d82652eb55947
rrc <raul.elfriky@gmail.com>
2014-12-27 21:45:09 +0100

    Well formatted

--- report2.py
+++ report2.py
@@ -1,52 +1,43 @@
-rows = lambda x: "<td>" + str(x) + "</td>"
-
-C1=0
-C2=1
-C3=2
-C4=3
-C5=4
-
-columns = [C1,C2,C3,C4,C5]
-
-p1 = [11,12,13,14,15]
-p2 = [21,22,23,24,25]
-p3 = [31,32,33,34,35]
-reports = [p1,p2,p3]
-
+#HTML tags
 table_h = "<table>"
 table_f = "</table>"
 row_h = "<tr>"
 row_f = "</tr>"
+cell_h = "<td>"
+cell_f = "</td>"
 
-row1 = lambda x: x[C1]
-row2 = lambda x: x[C2]
-row3 = lambda x: x[C3]
-row4 = lambda x: x[C4]
-row5 = lambda x: x[C5]
+#Rows index
+ROW1=0
+ROW2=1
+ROW3=2
+ROW4=3
+ROW5=4
+
+#Reports
+r1 = [11,12,13,14,15]
+r2 = [21,22,23,24,25]
+r3 = [31,32,33,34,35]
+reports = [r1,r2,r3]
+
+single_cell = lambda x: cell_h + str(x) + cell_f
+
+function_row1 = lambda x: x[ROW1]
+function_row2 = lambda x: x[ROW2]
+function_row3 = lambda x: x[ROW3]
+function_row4 = lambda x: x[ROW4]
+function_row5 = lambda x: x[ROW5]
 
 #row = lambda x,row: x[row]
 
-row_1 = map(row1, reports)
-row_2 = map(row2, reports)
-row_3 = map(row3, reports)
-row_4 = map(row4, reports)
-row_5 = map(row5, reports)
+row_1 = map(function_row1, reports)
+row_2 = map(function_row2, reports)
+row_3 = map(function_row3, reports)
+row_4 = map(function_row4, reports)
+row_5 = map(function_row5, reports)
 
 all_rows = [row_1, row_2, row_3, row_4, row_5]
 
-#row_3 = map(row, reports,2)
-
-#print row_1
-#print row_2
-#print row_3
-#result_single_row = lambda x: table_h + row_h + ''.join(map(rows, x)) + row_f +
-
-result_single_row = lambda x: row_h + ''.join(map(rows, x)) + row_f
+result_single_row = lambda x: row_h + ''.join(map(single_cell, x)) + row_f
 result_final = lambda x: table_h + ''.join(map(result_single_row, x)) + table_f
 
-#print result_single_row(row_1)
 print result_final(all_rows)
-
-#final_table = lambda x: table_h + ''.join(map(result_single_row(result_single_row, x))) + table_f
-#res = final_table(rows_values)
-#print ''.join(map(result_row_C1,reports))
