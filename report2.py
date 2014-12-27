4ce65e9062e4a14df5cb1a2936c4897fa41aa8df
rrc <raul.elfriky@gmail.com>
2014-12-27 21:22:12 +0100

    Stable - Final table stable

--- report2.py
+++ report2.py
@@ -20,18 +20,24 @@ row_f = "</tr>"
 
 row1 = lambda x: x[C1]
 row2 = lambda x: x[C2]
+row3 = lambda x: x[C3]
+row4 = lambda x: x[C4]
+row5 = lambda x: x[C5]
 
 #row = lambda x,row: x[row]
 
 row_1 = map(row1, reports)
 row_2 = map(row2, reports)
+row_3 = map(row3, reports)
+row_4 = map(row4, reports)
+row_5 = map(row5, reports)
 
-all_rows = [row_1, row_2]
+all_rows = [row_1, row_2, row_3, row_4, row_5]
 
 #row_3 = map(row, reports,2)
 
-print row_1
-print row_2
+#print row_1
+#print row_2
 #print row_3
 #result_single_row = lambda x: table_h + row_h + ''.join(map(rows, x)) + row_f +
 
